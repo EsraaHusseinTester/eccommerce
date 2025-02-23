c552cefc80ec65dd49b2af4710331e7f7fd2c6b3
Esraa Hussein Abdelaaty <esraahussein848@gmail.com>
2025-02-23 18:56:18 +0200

    most sold product

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./css/style.css";
 import '@fortawesome/fontawesome-free/js/all.min';
 
-document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item =>new bootstrap.Tooltip(item) )
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item =>new bootstrap.Tooltip(item))
+document.querySelectorAll('.add-to-card-btn').forEach(item =>{
+    item.addEventListener("click", () =>{
+        alert("أضيف المنتج إلى عربة الشراء")
+    })
+})
+
 console.log("أهلًا بك في متجر عربي")
 console.log("أهلًا بك في أكاديمية حسوب")
