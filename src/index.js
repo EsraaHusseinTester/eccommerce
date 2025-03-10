16938fc9b569fe7473bb54f0c5106281225c3f3a
Esraa Hussein Abdelaaty <esraahussein848@gmail.com>
2025-03-11 01:20:33 +0200

    product page done

--- src/index.js
+++ src/index.js
@@ -10,6 +10,25 @@ document.querySelectorAll('.add-to-card-btn').forEach(item =>{
     })
 })
 
+
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active');
+        });
+        item.parentNode.parentNode.classList.add('active')
+    });
+});
+
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active');
+        });
+        item.parentNode.parentNode.classList.add('active')
+    });
+});
+
 document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة"  + new Date().getFullYear();
 
 console.log("أهلًا بك في متجر عربي")
